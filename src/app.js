d813f460b6874f92aaaac2bee41d16fc9d3b951d
Derek Chan <derekchan7@gmail.com>
2018-12-06 01:39:43 -0500

    Sort seats.json before use

--- src/app.js
+++ src/app.js
@@ -4,6 +4,20 @@ import Data from './data/seats'
 import Seat from './Seat';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      // Initialize JSON data sorted by row # then letter assignment
+      data: Data.sort((a, b) => {
+        if(a['row'] === b['row']) {
+          return a['seat'] === b['seat'] ? 0 : a['seat'] < b['seat'] ? -1 : 1;
+        } else {
+          return a['row'] < b['row'] ? -1 : 1;
+        }
+      })
+    };
+  }
+
   render() {
     return (
       <Seat />
